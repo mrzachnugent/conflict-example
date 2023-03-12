df6fcddd0c48bfe4bed8d83faf328b85d0496ec0
szautkin <szautkin@gmail.com>
2023-03-12 09:59:40 -0500

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <h1>No External UI libraries</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
+          test is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
